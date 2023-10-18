50c0cc9f796edc3ba25db80345fbc3be90a69f85
LTAP220501 <ltap220501@Devboxs-MacBook-Pro-2.local>
2023-10-18 19:13:29 +0500

    Y

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2" # Choose your desired region
+}
+
+module "vpc" {
+  source = "./vpc_moddule" # The path to your vpc.tf file
+}
+
+module "ec2" {
+  source = "./ec2_module" # The path to your ec2.tf file
+  vpc_id = module.vpc.vpc_id # Reference the output variable from the vpc module
+}
