bb81cd0cc7e6888f167ad7e506bd947b5e10d58c
root <root@ip-172-31-4-139.us-east-2.compute.internal>
2023-10-19 08:07:36 +0000

    changes to output file

--- terraform/ec2_module/outputs.tf
+++ terraform/ec2_module/outputs.tf
@@ -2,3 +2,7 @@ output "ec2_instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.rails_instance.id
 }
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.rails_instance.public_ip
+}
