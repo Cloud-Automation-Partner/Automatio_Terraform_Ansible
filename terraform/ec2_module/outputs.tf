a8fa975f52513a4d286874fb8773d462417a3ce2
LTAP220501 <ltap220501@Devboxs-MacBook-Pro-2.local>
2023-10-24 12:23:45 +0500

    changes

--- terraform/ec2_module/outputs.tf
+++ terraform/ec2_module/outputs.tf
@@ -2,11 +2,11 @@ output "ec2_instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.rails_instance.id
 }
-output "ec2_public_ip" {
+output "app_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.rails_instance.public_ip
 }
-output "ec2_public_ip" {
+output "db_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.db_instance_instance.public_ip
+  value       = aws_instance.db_instance.public_ip
 }
